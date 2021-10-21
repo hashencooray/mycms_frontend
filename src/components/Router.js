39aa83ba522271f3789903602041fa5b48258a62
hashencooray <hashenrashminda@gmail.com>
2021-10-21 16:08:00 +0530

    User-Table

--- src/components/Router.js
+++ src/components/Router.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import AddEditUser from "./AddEditUser";
 import Header from "./Header";
+import UsersTable from "./UsersTable";
 
 export default function Router() {
   return (
@@ -12,6 +13,9 @@ export default function Router() {
           </Route>
           <Route exact path="/user/header">
             <Header />
+          </Route>
+          <Route exact path="/user/users-table">
+            <UsersTable />
           </Route>
       </Switch>
     </BrowserRouter>
