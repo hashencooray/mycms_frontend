019772d7b56efac454f86f79aa90c03331791fde
hashencooray <hashenrashminda@gmail.com>
2021-10-21 14:29:59 +0530

    header routing

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import AddEditUser from "./AddEditUser";
+import Header from "./Header";
 
 export default function Router() {
   return (
@@ -8,6 +9,9 @@ export default function Router() {
       <Switch>
           <Route exact path="/user/add">
             <AddEditUser />
+          </Route>
+          <Route exact path="/user/header">
+            <Header />
           </Route>
       </Switch>
     </BrowserRouter>
